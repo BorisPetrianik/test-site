9563fae8b8e390fde54c7d12cb54fa84b8471e0b
BorisPetrianik <borispetrianik@gmail.com>
2019-08-08 02:56:47 +0300

    Now open and click on "Get started for free" button.

---
+++ src/test/java/com/wrike/WebDriverSettings.java
@@ -0,0 +1,21 @@
+package com.wrike;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverSettings {
+    public ChromeDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/chromedriver");
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void close() {
+        driver.quit();
+    }
+
+}
