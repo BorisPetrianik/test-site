ee1eb7e5a1449aba462d72f8777c7b76fa895b0e
BorisPetrianik <borispetrianik@gmail.com>
2019-08-08 04:04:36 +0300

    Now can see the registration form and input sample email(Need opening for mobile version and random email generator)

--- src/test/java/com/wrike/FirstTest.java
+++ src/test/java/com/wrike/FirstTest.java
@@ -1,12 +1,10 @@
 package com.wrike;
 
-import junit.framework.Assert;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.junit.Assert.assertTrue;
 
@@ -18,10 +16,13 @@ public class FirstTest extends WebDriverSettings {
         String title = driver.getTitle();
         assertTrue(title.equals("Your online project management software - Wrike"));
 
-        WebElement button = driver.findElement(By.cssSelector("body > div.wg-layout.wg-layout--outline > header > div.wg-header__desktop > div.wg-header__sticky-menu > div > div > div.wg-header__cell.wg-header__cell--xs-3.wg-header__cell--sm-6.wg-header__cell--md-5.wg-header__cell--xl-5 > div > form > button"));
+        WebDriverWait wait = new WebDriverWait(driver, 2);
 
-        button.click();
+        WebElement dbutton = driver.findElement(By.cssSelector("body > div.wg-layout.wg-layout--outline > header > div.wg-header__desktop > div.wg-header__sticky-menu > div > div > div.wg-header__cell.wg-header__cell--xs-3.wg-header__cell--sm-6.wg-header__cell--md-5.wg-header__cell--xl-5 > div > form > button"));
+        dbutton.click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"modal-pro\"]/form")));
+
+        driver.findElement(By.xpath("//*[@id=\"modal-pro\"]/form/label[1]/input")).sendKeys("flexAir2_0@gmail.com");
     }
-
 
 }
--- src/test/java/com/wrike/WebDriverSettings.java
+++ src/test/java/com/wrike/WebDriverSettings.java
@@ -17,5 +17,4 @@ public class WebDriverSettings {
     public void close() {
         driver.quit();
     }
-
 }
